a5fe8cddcba3cd9253d5553fa502da46957d7005
VladKopalov <vladkop@mail.ru>
2020-09-21 13:39:33 +0300

    update dao

--- dev/run.php
+++ dev/run.php
@@ -6,7 +6,7 @@ use Wumvi\Sqlite3Dao\DbManager;
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$dbManager = new DbManager(__DIR__ . '/recall-request.sqlite');
+$dbManager = new DbManager(__DIR__ . '/recall-request.sqlite', 'wal');
 $recallRequestDao = new ReCallRequestDao($dbManager);
 $recallRequestService = new ReCallRequestService($recallRequestDao);
 // $recallRequestService->add('http://localhost:8837', 'POST', '');
